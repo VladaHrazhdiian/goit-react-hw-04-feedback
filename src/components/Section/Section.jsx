635f56c910ac7892170905e9e0932e9da35fd954
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-09-11 22:26:29 +0300

    work

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,6 @@
 import { SectionBox, Title } from './Section.styled';
+import PropTypes from 'prop-types';
+
 const Section = ({ title, children }) => {
     return (
         <SectionBox>
@@ -7,5 +9,10 @@ const Section = ({ title, children }) => {
         </SectionBox>
     );
 };
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.element,
+}
 
 export default Section;
